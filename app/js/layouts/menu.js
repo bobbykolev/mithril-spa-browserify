656e6f686c692dbb119c2768bff012f0b3cf4d2e
bobbykolev <bobbykolev@abv.bg>
2015-06-09 16:10:46 +0300

    active state of nav links was implemented

--- app/js/layouts/menu.js
+++ app/js/layouts/menu.js
@@ -3,16 +3,24 @@ var m = require('../../../libs/mithril/mithril.min');
 var menu = {};
 
 menu.controller = function() {
+	this.setActiveClass = function (txt) {
+		console.log(m.route())
+		if (m.route() && m.route() != '/') {
+			return  m.route().indexOf(txt) > -1 ? 'active':'';
+		} else if (txt == 'home') {
+			return  'active';
+		}
+	};
 };
 
-menu.view = function() {
+menu.view = function(ctrl) {
   return m("nav", [
   		m('ul', [
 	  		m('li', [
-	  			m("a.home", {href: "/home", config: m.route}, "Home")
+	  			m("a", { class: ctrl.setActiveClass('home'), href: "/home", config: m.route }, "Home")
 			]),
 			m('li', [
-	  			m("a.posts", {href: "/posts", config: m.route}, "Posts")
+	  			m("a", { class: ctrl.setActiveClass('posts'), href: "/posts", config: m.route }, "Posts")
 			])
 		])
   ]);
